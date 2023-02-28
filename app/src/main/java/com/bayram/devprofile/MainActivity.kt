ab53b52a4d0e75e459a052de8256cdc7645197b1
Bayram-Jm <bjumayew@gmail.com>
2023-02-28 16:57:58 +0500

    Tablet version added

--- app/src/main/java/com/bayram/devprofile/MainActivity.kt
+++ app/src/main/java/com/bayram/devprofile/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.bayram.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.bayram.devprofile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
+
+        // Corner for logo
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        binding.logo.setImageDrawable(rounded)
+
     }
 }
